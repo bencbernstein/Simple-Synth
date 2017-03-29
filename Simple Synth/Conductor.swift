2778179f5e61aa36884c7c7ecac316bf18c1b4f6
Ben Bernstein <bb@burningflowe.rs>
2017-03-29 16:54:37 -0400

    conductor class and reverb

--- Simple Synth/Conductor.swift
+++ Simple Synth/Conductor.swift
@@ -7,14 +7,72 @@
 //
 
 import Foundation
+import AudioKit
 
-class Conductor {
+final class Conductor {
+    
     static let sharedInstance = Conductor()
     
+    let osc1 = AKOscillator()
+    let osc2 = AKOscillator()
+    let osc3 = AKOscillator()
+    let osc4 = AKOscillator()
+    let osc5 = AKOscillator()
+    var tones = [AKAmplitudeEnvelope]()
+    
+    var envelope1: AKAmplitudeEnvelope
+    var envelope2: AKAmplitudeEnvelope
+    var envelope3: AKAmplitudeEnvelope
+    var envelope4: AKAmplitudeEnvelope
+    var envelope5: AKAmplitudeEnvelope
+    
+    var reverb: AKCostelloReverb
+    var reverbMixer: AKDryWetMixer
+    
+    var masterVolume = AKMixer()
+    
     init () {
-    
-    
-    
+        
+        // oscillators
+        let baseFreq = 300.0
+        osc1.frequency = baseFreq
+        osc2.frequency = baseFreq * 27/24
+        osc3.frequency = baseFreq * 30/24
+        osc4.frequency = baseFreq * 36/24
+        osc5.frequency = baseFreq * 40/24
+        
+        let oscillators = [osc1, osc2, osc3, osc4, osc5]
+        oscillators.forEach {$0.amplitude = 0.5; $0.start() }
+        
+        // wrap them in amplitude envelopes
+        envelope1 = AKAmplitudeEnvelope(osc1)
+        envelope2 = AKAmplitudeEnvelope(osc2)
+        envelope3 = AKAmplitudeEnvelope(osc3)
+        envelope4 = AKAmplitudeEnvelope(osc4)
+        envelope5 = AKAmplitudeEnvelope(osc5)
+        
+        let envelopes = [envelope1, envelope2, envelope3, envelope4, envelope5]
+        
+        tones = envelopes
+
+        tones.forEach {
+            $0.attackDuration = 0.01
+            $0.decayDuration = 0.1
+            $0.sustainLevel = 0.3
+            $0.releaseDuration = 0.3
+        }
+
+        masterVolume = AKMixer(envelope1, envelope2, envelope3, envelope4, envelope5)
+
+        reverb = AKCostelloReverb(masterVolume)
+        reverb.play()
+        
+        reverbMixer = AKDryWetMixer(masterVolume, reverb, balance: 2.0)
+
+        AudioKit.output = reverbMixer
+        AudioKit.start()
+        
+        
     }
     
     
--- Simple Synth/ViewController.swift
+++ Simple Synth/ViewController.swift
@@ -11,24 +11,13 @@ import AudioKit
 
 class ViewController: UIViewController {
     
+    let conductor = Conductor.sharedInstance
+    
     @IBOutlet weak var button5: UIButton!
     @IBOutlet weak var button4: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button1: UIButton!
-    
-    let osc1 = AKOscillator()
-    let osc2 = AKOscillator()
-    let osc3 = AKOscillator()
-    let osc4 = AKOscillator()
-    let osc5 = AKOscillator()
-    var tones = [AKAmplitudeEnvelope]()
-    var envelope1: AKAmplitudeEnvelope?
-    var envelope2: AKAmplitudeEnvelope?
-    var envelope3: AKAmplitudeEnvelope?
-    var envelope4: AKAmplitudeEnvelope?
-    var envelope5: AKAmplitudeEnvelope?
-    var mixer: AKMixer?
     
     @IBAction func soundButton(_ sender: UIButton) {
         toggleSound(pad: sender)
@@ -36,67 +25,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Minor pentatonic just intation 24:27:30:36:40:48
-        
-        let baseFreq = 200.0
-        osc1.frequency = baseFreq
-        osc1.rampTime = 0.2
-        osc1.amplitude = 0.5
-        osc1.start()
-        
-        osc2.frequency = baseFreq * 27/24
-        osc2.amplitude = 0.5
-        osc2.start()
-        
-        osc3.frequency = baseFreq * 30/24
-        osc3.amplitude = 0.5
-        osc3.start()
-        
-        osc4.frequency = baseFreq * 36/24
-        osc4.amplitude = 0.5
-        osc4.start()
-        
-        osc5.frequency = baseFreq * 40/24
-        osc5.amplitude = 0.5
-        osc5.start()
-        
-        envelope1 = AKAmplitudeEnvelope(osc1)
-        envelope2 = AKAmplitudeEnvelope(osc2)
-        envelope3 = AKAmplitudeEnvelope(osc3)
-        envelope4 = AKAmplitudeEnvelope(osc4)
-        envelope5 = AKAmplitudeEnvelope(osc5)
-        
-        tones.append(envelope1!)
-        tones.append(envelope2!)
-        tones.append(envelope3!)
-        tones.append(envelope4!)
-        tones.append(envelope5!)
-        
-            envelope1?.attackDuration = 0.01
-            envelope1?.decayDuration = 0.1
-            envelope1?.sustainLevel = 0.4
-            envelope1?.releaseDuration = 0.5
-
-
-        mixer = AKMixer(envelope1, envelope2, envelope3, envelope4, envelope5)
-
-        AudioKit.output = mixer
-        AudioKit.start()
-        
         setupButtons()
-      
     }
     
     func toggleSound(pad: UIButton) {
-        self.tones[pad.tag].start()
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
-            self.tones[pad.tag].stop()
-            
-        }
+        // TODO: maybe make a new tone, play it, remove it ...
         
-
+        conductor.tones[pad.tag].start()
+        pad.backgroundColor = UIColor.lightGray
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.conductor.tones[pad.tag].stop()
+            pad.backgroundColor = self.colors[pad.tag]
+        }
     }
+    
+    let colors = [0: UIColor.blue, 1: UIColor.green, 2:UIColor.yellow, 3:UIColor.brown, 4:UIColor.cyan]
     
     func setupButtons() {
         button1.backgroundColor = UIColor.blue

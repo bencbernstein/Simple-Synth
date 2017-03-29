f7f5665f723a8dc887413982db69c6b2c57223ba
Ben Bernstein <bb@burningflowe.rs>
2017-03-29 14:55:10 -0400

    works

--- Simple Synth/ViewController.swift
+++ Simple Synth/ViewController.swift
@@ -21,11 +21,13 @@ class ViewController: UIViewController {
     let osc2 = AKOscillator()
     let osc3 = AKOscillator()
     let osc4 = AKOscillator()
+    let osc5 = AKOscillator()
     var tones = [AKAmplitudeEnvelope]()
     var envelope1: AKAmplitudeEnvelope?
     var envelope2: AKAmplitudeEnvelope?
     var envelope3: AKAmplitudeEnvelope?
     var envelope4: AKAmplitudeEnvelope?
+    var envelope5: AKAmplitudeEnvelope?
     var mixer: AKMixer?
     
     @IBAction func soundButton(_ sender: UIButton) {
@@ -34,41 +36,50 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        osc1.frequency = 220
+        
+        //Minor pentatonic just intation 24:27:30:36:40:48
+        
+        let baseFreq = 200.0
+        osc1.frequency = baseFreq
+        osc1.rampTime = 0.2
         osc1.amplitude = 0.5
         osc1.start()
         
-        osc2.frequency = 440
+        osc2.frequency = baseFreq * 27/24
         osc2.amplitude = 0.5
         osc2.start()
         
-        osc3.frequency = 660
+        osc3.frequency = baseFreq * 30/24
         osc3.amplitude = 0.5
         osc3.start()
         
-        osc4.frequency = 840
+        osc4.frequency = baseFreq * 36/24
         osc4.amplitude = 0.5
         osc4.start()
+        
+        osc5.frequency = baseFreq * 40/24
+        osc5.amplitude = 0.5
+        osc5.start()
         
         envelope1 = AKAmplitudeEnvelope(osc1)
         envelope2 = AKAmplitudeEnvelope(osc2)
         envelope3 = AKAmplitudeEnvelope(osc3)
         envelope4 = AKAmplitudeEnvelope(osc4)
+        envelope5 = AKAmplitudeEnvelope(osc5)
         
         tones.append(envelope1!)
         tones.append(envelope2!)
         tones.append(envelope3!)
         tones.append(envelope4!)
+        tones.append(envelope5!)
         
-        if let envelope = envelope1 {
-            envelope.attackDuration = 0.01
-            envelope.decayDuration = 0.1
-            envelope.sustainLevel = 0.4
-            envelope.releaseDuration = 0.5
+            envelope1?.attackDuration = 0.01
+            envelope1?.decayDuration = 0.1
+            envelope1?.sustainLevel = 0.4
+            envelope1?.releaseDuration = 0.5
 
-        }
 
-        mixer = AKMixer(envelope1, envelope2, envelope3, envelope4)
+        mixer = AKMixer(envelope1, envelope2, envelope3, envelope4, envelope5)
 
         AudioKit.output = mixer
         AudioKit.start()
@@ -78,7 +89,12 @@ class ViewController: UIViewController {
     }
     
     func toggleSound(pad: UIButton) {
-        tones[pad.tag].isPlaying ? tones[pad.tag].stop() : tones[pad.tag].start()
+        self.tones[pad.tag].start()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+            self.tones[pad.tag].stop()
+            
+        }
+        
 
     }
     
